ac6fc5c5ef70f01c729f690ef373ea1647c64d99
我先逃 <benight169@gmail.com>
2026-04-16 15:08:28 +0800

    Delete assets directory

--- assets/js/tailwind-config.js
+++
@@ -1,9 +0,0 @@
-tailwind.config = {
-            theme: {
-                extend: {
-                    colors: { tomato: '#E35453', leaf: '#24992B', black: '#0A0A0A', offwhite: '#F9F9F9' },
-                    fontFamily: { sans: ['"Helvetica Neue"', 'Helvetica', 'Arial', '"PingFang SC"', '"Hiragino Sans GB"', '"Microsoft YaHei"', 'sans-serif'] },
-                    letterSpacing: { tight: '-.02em', widest: '.2em' }
-                }
-            }
-        }
